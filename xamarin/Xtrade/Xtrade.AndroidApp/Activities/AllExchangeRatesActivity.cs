93dbce32a325122db5de2f97faa4e936f7d3a769
Joshua Fenemore <joshuaf@datacom.co.nz>
2016-10-21 10:48:51 +1300

    Labels update, run on ui thread

--- xamarin/Xtrade/Xtrade.AndroidApp/Activities/AllExchangeRatesActivity.cs
+++ xamarin/Xtrade/Xtrade.AndroidApp/Activities/AllExchangeRatesActivity.cs
@@ -13,15 +13,15 @@
     using Shared.Interfaces.ViewModels;
     using Toolbar = Android.Support.V7.Widget.Toolbar;
 
-    [Activity(Label = "@string/allRatesLabel", Theme = "@style/Xtrade", MainLauncher = true, Icon = "@mipmap/ic_launcher")]
+    [Activity(Label = "@string/applicationName", Theme = "@style/Xtrade", MainLauncher = true, Icon = "@mipmap/ic_launcher")]
     public class AllExchangeRatesActivity : BaseActivity<IAllRatesViewModel>
     {
-        private Toolbar applicationToolbar;
-        private TextView noRatesTextView;
-        private SwipeRefreshLayout swipeRefreshLayout;
-        private RecyclerView ratesRecyclerView;
-        private RecyclerView.LayoutManager ratesRecylerViewLayoutManager;
-        private RatesRecyclerAdapter ratesRecyclerAdapter;
+        private Toolbar _applicationToolbar;
+        private TextView _noRatesTextView;
+        private SwipeRefreshLayout _swipeRefreshLayout;
+        private RecyclerView _ratesRecyclerView;
+        private RecyclerView.LayoutManager _ratesRecylerViewLayoutManager;
+        private RatesRecyclerAdapter _ratesRecyclerAdapter;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -29,13 +29,14 @@
 
             this.SetContentView(Resource.Layout.activity_all_exchange_rates);
 
-            this.applicationToolbar = this.FindViewById<Toolbar>(Resource.Id.applicationToolbar);
-            this.noRatesTextView = this.FindViewById<TextView>(Resource.Id.noRatesTextView);
-            this.swipeRefreshLayout = this.FindViewById<SwipeRefreshLayout>(Resource.Id.swipeRefreshLayout);
-            this.ratesRecyclerView = this.FindViewById<RecyclerView>(Resource.Id.ratesRecyclerView);
-            this.ratesRecylerViewLayoutManager = new LinearLayoutManager(this);
+            this._applicationToolbar = this.FindViewById<Toolbar>(Resource.Id.applicationToolbar);
+            this._noRatesTextView = this.FindViewById<TextView>(Resource.Id.noRatesTextView);
+            this._swipeRefreshLayout = this.FindViewById<SwipeRefreshLayout>(Resource.Id.swipeRefreshLayout);
+            this._ratesRecyclerView = this.FindViewById<RecyclerView>(Resource.Id.ratesRecyclerView);
+            this._ratesRecylerViewLayoutManager = new LinearLayoutManager(this);
 
-            this.SetSupportActionBar(this.applicationToolbar);
+            this.SetSupportActionBar(this._applicationToolbar);
+            this._applicationToolbar.SetTitle(Resource.String.allRatesLabel);
         }
 
         protected override void OnResume()
@@ -46,9 +47,9 @@
             this.ViewModel.OnRefreshError += this.ViewModelRefreshError;
             this.ViewModel.OnRefreshSuccess += this.ViewModelRefreshSuccess;
 
-            this.swipeRefreshLayout.Refresh += this.SwipeRefreshLayoutOnRefresh;
+            this._swipeRefreshLayout.Refresh += this.SwipeRefreshLayoutOnRefresh;
 
-            this.swipeRefreshLayout.Refreshing = true;
+            this._swipeRefreshLayout.Refreshing = true;
             this.ViewModel.LoadData();
         }
         
@@ -60,63 +61,73 @@
             this.ViewModel.OnRefreshError -= this.ViewModelRefreshError;
             this.ViewModel.OnRefreshSuccess -= this.ViewModelRefreshSuccess;
 
-            this.swipeRefreshLayout.Refresh -= this.SwipeRefreshLayoutOnRefresh;
+            this._swipeRefreshLayout.Refresh -= this.SwipeRefreshLayoutOnRefresh;
         }
 
         private void UpdateViews()
         {
-            this.swipeRefreshLayout.Refreshing = this.ViewModel.IsDataRefreshing;
+
+            this._swipeRefreshLayout.Refreshing = this.ViewModel.IsDataRefreshing;
 
             if (this.ViewModel.AllRates == null || this.ViewModel.AllRates.Count == 0)
             {
-                this.noRatesTextView.Visibility = ViewStates.Visible;
-                this.ratesRecyclerView.Visibility = ViewStates.Gone;
+                this._noRatesTextView.Visibility = ViewStates.Visible;
+                this._ratesRecyclerView.Visibility = ViewStates.Gone;
             }
             else
             {
-                this.noRatesTextView.Visibility = ViewStates.Gone;
-                this.ratesRecyclerView.Visibility = ViewStates.Visible;
+                this._noRatesTextView.Visibility = ViewStates.Gone;
+                this._ratesRecyclerView.Visibility = ViewStates.Visible;
 
-                if (this.ratesRecyclerAdapter == null)
+                if (this._ratesRecyclerAdapter == null)
                 {
-                    this.ratesRecyclerAdapter = new RatesRecyclerAdapter(this, this.ViewModel.AllRates, i =>
+                    this._ratesRecyclerAdapter = new RatesRecyclerAdapter(this, this.ViewModel.AllRates, i =>
                     {
-                        Intent detailsIntent = new Intent(this, typeof(ExchangeRateDetailsActivity));
+                        Intent detailsIntent = new Intent(this, typeof (ExchangeRateDetailsActivity));
                         detailsIntent.PutExtra(Helpers.AndroidConstants.SelectedRateCode, this.ViewModel.AllRates[i].CurrencyCode);
                         this.StartActivity(detailsIntent);
                     });
 
-                    this.ratesRecyclerView.SetAdapter(this.ratesRecyclerAdapter);
-                    this.ratesRecyclerView.SetLayoutManager(this.ratesRecylerViewLayoutManager);
+                    this._ratesRecyclerView.SetAdapter(this._ratesRecyclerAdapter);
+                    this._ratesRecyclerView.SetLayoutManager(this._ratesRecylerViewLayoutManager);
                 }
                 else
                 {
-                    this.ratesRecyclerAdapter.UpdateDataSet(this.ViewModel.AllRates);
+                    this._ratesRecyclerAdapter.UpdateDataSet(this.ViewModel.AllRates);
                 }
             }
         }
 
         private void SwipeRefreshLayoutOnRefresh(object sender, EventArgs eventArgs)
         {
-            this.swipeRefreshLayout.Refreshing = true;
+            this._swipeRefreshLayout.Refreshing = true;
             this.ViewModel.RefreshRates();
         }
 
         private void ViewModelDataChanged(object sender, EventArgs eventArgs)
         {
-            this.UpdateViews();
+            this.RunOnUiThread(() =>
+            {
+                this.UpdateViews(); 
+            });
         }
 
         private void ViewModelRefreshError(object sender, string s)
         {
-            this.swipeRefreshLayout.Refreshing = this.ViewModel.IsDataRefreshing;
-            Snackbar.Make(this.swipeRefreshLayout, s, Snackbar.LengthLong).Show();
+            this.RunOnUiThread(() =>
+            {
+                this._swipeRefreshLayout.Refreshing = this.ViewModel.IsDataRefreshing;
+                Snackbar.Make(this._swipeRefreshLayout, s, Snackbar.LengthLong).Show();
+            });
         }
 
         private void ViewModelRefreshSuccess(object sender, string s)
         {
-            this.swipeRefreshLayout.Refreshing = this.ViewModel.IsDataRefreshing;
-            Snackbar.Make(this.swipeRefreshLayout, s, Snackbar.LengthLong).Show();
+            this.RunOnUiThread(() =>
+            {
+                this._swipeRefreshLayout.Refreshing = this.ViewModel.IsDataRefreshing;
+                Snackbar.Make(this._swipeRefreshLayout, s, Snackbar.LengthLong).Show();
+            });
         }
     }
 }
