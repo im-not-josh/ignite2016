625bba16080bd5dd9edb234fea695e9f0d75ab49
Joshua Fenemore <joshuaf@datacom.co.nz>
2016-10-21 10:37:01 +1300

    Adding to details view

--- xamarin/Xtrade/Xtrade.AndroidApp/Activities/ExchangeRateDetailsActivity.cs
+++ xamarin/Xtrade/Xtrade.AndroidApp/Activities/ExchangeRateDetailsActivity.cs
@@ -13,11 +13,17 @@
     [Activity(Label = "@string/exchangeRateDetails", Theme = "@style/Xtrade")]
     public class ExchangeRateDetailsActivity : BaseActivity<ISelectedRateViewModel>
     {
-        private Toolbar applicationToolbar;
-        private SwipeRefreshLayout swipeRefreshLayout;
-        private TextView forexCodeTextView;
-        private TextView forexCountryTextView;
-        private ImageView forexFlagImageView;
+        private Toolbar _applicationToolbar;
+        private SwipeRefreshLayout _swipeRefreshLayout;
+        private TextView _forexCodeTextView;
+        private TextView _forexCountryTextView;
+        private ImageView _forexFlagImageView;
+        private TextView _buyNotesTextView;
+        private TextView _buyChequesTextView;
+        private TextView _buyPaymentsTextView;
+        private TextView _sellsNotesTextView;
+        private TextView _smallestNotesTextViewextView;
+
         private string _selectedRateCode;
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -26,13 +32,18 @@
 
             this.SetContentView(Resource.Layout.activity_exchange_rate_details);
 
-            this.applicationToolbar = this.FindViewById<Toolbar>(Resource.Id.applicationToolbar);
-            this.swipeRefreshLayout = this.FindViewById<SwipeRefreshLayout>(Resource.Id.swipeRefreshLayout);
-            this.forexCodeTextView = this.FindViewById<TextView>(Resource.Id.forexCodeTextView);
-            this.forexCountryTextView = this.FindViewById<TextView>(Resource.Id.forexCountryTextView);
-            this.forexFlagImageView = this.FindViewById<ImageView>(Resource.Id.forexFlagImageView);
+            this._applicationToolbar = this.FindViewById<Toolbar>(Resource.Id.applicationToolbar);
+            this._swipeRefreshLayout = this.FindViewById<SwipeRefreshLayout>(Resource.Id.swipeRefreshLayout);
+            this._forexCodeTextView = this.FindViewById<TextView>(Resource.Id.forexCodeTextView);
+            this._forexCountryTextView = this.FindViewById<TextView>(Resource.Id.forexCountryTextView);
+            this._forexFlagImageView = this.FindViewById<ImageView>(Resource.Id.forexFlagImageView);
+            this._buyNotesTextView = this.FindViewById<TextView>(Resource.Id.buyNotesTextView);
+            this._buyChequesTextView = this.FindViewById<TextView>(Resource.Id.buyChequesTextView);
+            this._buyPaymentsTextView = this.FindViewById<TextView>(Resource.Id.buyPaymentsTextView);
+            this._sellsNotesTextView = this.FindViewById<TextView>(Resource.Id.sellsNotesTextView);
+            this._smallestNotesTextViewextView = this.FindViewById<TextView>(Resource.Id.smallestNoteTextView);
 
-            this.SetSupportActionBar(this.applicationToolbar);
+            this.SetSupportActionBar(this._applicationToolbar);
             this.SupportActionBar.SetDisplayHomeAsUpEnabled(true);
 
             Bundle bundle = this.Intent.Extras ?? savedInstanceState;
@@ -72,9 +83,9 @@
             this.ViewModel.OnRefreshError += this.ViewModelRefreshError;
             this.ViewModel.OnRefreshSuccess += this.ViewModelRefreshSuccess;
 
-            this.swipeRefreshLayout.Refresh += this.SwipeRefreshLayoutOnRefresh;
+            this._swipeRefreshLayout.Refresh += this.SwipeRefreshLayoutOnRefresh;
 
-            this.swipeRefreshLayout.Refreshing = true;
+            this._swipeRefreshLayout.Refreshing = true;
             this.ViewModel.LoadData(this._selectedRateCode);
         }
         
@@ -86,7 +97,7 @@
             this.ViewModel.OnRefreshError -= this.ViewModelRefreshError;
             this.ViewModel.OnRefreshSuccess -= this.ViewModelRefreshSuccess;
 
-            this.swipeRefreshLayout.Refresh -= this.SwipeRefreshLayoutOnRefresh;
+            this._swipeRefreshLayout.Refresh -= this.SwipeRefreshLayoutOnRefresh;
         }
 
         protected override void OnSaveInstanceState(Bundle outgoingState)
@@ -97,27 +108,33 @@
 
         private void UpdateViews()
         {
-            this.swipeRefreshLayout.Refreshing = this.ViewModel.IsDataRefreshing;
+            this._swipeRefreshLayout.Refreshing = this.ViewModel.IsDataRefreshing;
 
-            this.forexCodeTextView.Text = this.ViewModel.SelectedRate.CurrencyCode;
-            this.forexCountryTextView.Text = this.ViewModel.SelectedRate.Description;
+            this._forexCodeTextView.Text = this.ViewModel.SelectedRate.CurrencyCode;
+            this._forexCountryTextView.Text = this.ViewModel.SelectedRate.Description;
 
             int flagID = this.Resources.GetIdentifier("flag_" + this.ViewModel.SelectedRate.CurrencyCode.ToLower(), "drawable", this.PackageName);
 
             if (flagID == 0)
             {
-                this.forexFlagImageView.Visibility = ViewStates.Gone;
+                this._forexFlagImageView.Visibility = ViewStates.Gone;
             }
             else
             {
-                this.forexFlagImageView.Visibility = ViewStates.Visible;
-                this.forexFlagImageView.SetImageDrawable(this.Resources.GetDrawable(flagID));
+                this._forexFlagImageView.Visibility = ViewStates.Visible;
+                this._forexFlagImageView.SetImageDrawable(this.Resources.GetDrawable(flagID));
             }
+
+            this._buyNotesTextView.Text = this.ViewModel.SelectedRate.BuysNotes.ToString("C");
+            this._buyChequesTextView.Text = this.ViewModel.SelectedRate.BuysCheques.ToString("C");
+            this._buyPaymentsTextView.Text = this.ViewModel.SelectedRate.BuysPayments.ToString("C");
+            this._sellsNotesTextView.Text = this.ViewModel.SelectedRate.SellsNotes.ToString("C");
+            this._smallestNotesTextViewextView.Text = this.ViewModel.SelectedRate.SmallestNote;
         }
 
         private void SwipeRefreshLayoutOnRefresh(object sender, EventArgs eventArgs)
         {
-            this.swipeRefreshLayout.Refreshing = true;
+            this._swipeRefreshLayout.Refreshing = true;
             this.ViewModel.RefreshSelectedRate();
         }
 
@@ -128,14 +145,14 @@
 
         private void ViewModelRefreshError(object sender, string s)
         {
-            this.swipeRefreshLayout.Refreshing = this.ViewModel.IsDataRefreshing;
-            Snackbar.Make(this.swipeRefreshLayout, s, Snackbar.LengthLong).Show();
+            this._swipeRefreshLayout.Refreshing = this.ViewModel.IsDataRefreshing;
+            Snackbar.Make(this._swipeRefreshLayout, s, Snackbar.LengthLong).Show();
         }
 
         private void ViewModelRefreshSuccess(object sender, string s)
         {
-            this.swipeRefreshLayout.Refreshing = this.ViewModel.IsDataRefreshing;
-            Snackbar.Make(this.swipeRefreshLayout, s, Snackbar.LengthLong).Show();
+            this._swipeRefreshLayout.Refreshing = this.ViewModel.IsDataRefreshing;
+            Snackbar.Make(this._swipeRefreshLayout, s, Snackbar.LengthLong).Show();
         }
     }
 }
